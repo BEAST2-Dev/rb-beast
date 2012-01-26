e49492c3416d446ee4dce38dd3587f66b308ed9a
higgs.ml@gmail.com <higgs.ml@gmail.com@ec332d28-3f27-85f1-ef98-7be09d9b0f74>
2012-01-26 20:20:26 +0000

    missing files

---
+++ trunk/src/beast/evolution/substitutionmodel/RB.java
@@ -0,0 +1,119 @@
+package beast.evolution.substitutionmodel;
+
+
+import java.util.Arrays;
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.Input.Validate;
+import beast.core.Valuable;
+import beast.core.parameter.IntegerParameter;
+import beast.evolution.datatype.DataType;
+import beast.evolution.datatype.Nucleotide;
+
+@Description("Reversible-jump Based substitution model for nucleotides that changes where the count input " +
+		"determines the number of parameters used in a hierarchy of models")
+public class RB extends GeneralSubstitutionModel {
+	public Input<IntegerParameter> m_countInput = new Input<IntegerParameter>("count", "model number used 0 = JC, 5 and higher if GTR (default 0)", Validate.REQUIRED);
+	
+	Valuable m_rate;
+	IntegerParameter m_count;
+	
+	@Override
+	public void initAndValidate() throws Exception {
+		m_count = m_countInput.get();
+		m_rate = m_rates.get();
+		if (m_rate.getDimension() != 5) {
+			throw new Exception("rate input must have dimension 5");
+		}
+		
+    	m_frequencies = frequenciesInput.get();
+        updateMatrix = true;
+        m_nStates = m_frequencies.getFreqs().length;
+    	if (m_nStates != 4) {
+    		throw new Exception("Frequencies has wrong size. Expected 4, but got " + m_nStates);
+    	}
+
+        eigenSystem = createEigenSystem();
+        m_rateMatrix = new double[m_nStates][m_nStates];
+        relativeRates = new double[m_nStates * (m_nStates - 1)];
+        storedRelativeRates = new double[m_nStates * (m_nStates - 1)];
+	}
+
+	@Override
+    protected void setupRelativeRates() {
+		switch (m_count.getValue()) {
+		case 0: // JC96
+			Arrays.fill(relativeRates, 1.0);
+			break;
+		case 1: // HKY
+	    	relativeRates[0] = m_rate.getArrayValue(0); // A->C
+	    	relativeRates[1] = 1; // A->G
+	    	relativeRates[2] = m_rate.getArrayValue(0); // A->T
+
+	    	relativeRates[4] = m_rate.getArrayValue(0); // C->G
+	    	relativeRates[5] = 1; // C->T
+
+	    	relativeRates[8] = m_rate.getArrayValue(0); // G->T
+			break;
+		case 2: // K3P
+	    	relativeRates[0] = m_rate.getArrayValue(0); // A->C
+	    	relativeRates[1] = m_rate.getArrayValue(1); // A->G
+	    	relativeRates[2] = m_rate.getArrayValue(0); // A->T
+
+	    	relativeRates[4] = m_rate.getArrayValue(0); // C->G
+	    	relativeRates[5] = 1; // C->T
+
+	    	relativeRates[8] = m_rate.getArrayValue(0); // G->T
+			break;
+		case 3: // TIM
+	    	relativeRates[0] = m_rate.getArrayValue(0); // A->C
+	    	relativeRates[1] = m_rate.getArrayValue(1); // A->G
+	    	relativeRates[2] = m_rate.getArrayValue(2); // A->T
+
+	    	relativeRates[4] = m_rate.getArrayValue(2); // C->G
+	    	relativeRates[5] = 1; // C->T
+
+	    	relativeRates[8] = m_rate.getArrayValue(0); // G->T
+			break;
+		case 4: // new model 
+	    	relativeRates[0] = m_rate.getArrayValue(0); // A->C
+	    	relativeRates[1] = m_rate.getArrayValue(1); // A->G
+	    	relativeRates[2] = m_rate.getArrayValue(2); // A->T
+
+	    	relativeRates[4] = m_rate.getArrayValue(3); // C->G
+	    	relativeRates[5] = 1; // C->T
+
+	    	relativeRates[8] = m_rate.getArrayValue(0); // G->T
+			break;
+		default: // GTR
+	    	relativeRates[0] = m_rate.getArrayValue(0); // A->C
+	    	relativeRates[1] = m_rate.getArrayValue(1); // A->G
+	    	relativeRates[2] = m_rate.getArrayValue(2); // A->T
+
+	    	relativeRates[4] = m_rate.getArrayValue(3); // C->G
+	    	relativeRates[5] = 1; // C->T
+
+	    	relativeRates[8] = m_rate.getArrayValue(4); // G->T
+			break;
+		}
+
+    	relativeRates[3] = relativeRates[0]; // C->A
+
+    	relativeRates[6] = relativeRates[1]; // G->A
+    	relativeRates[7] = relativeRates[4]; // G->C
+
+    	relativeRates[9] = relativeRates[2]; // T->A
+    	relativeRates[10] = 1; //T->C
+    	relativeRates[11] = relativeRates[8]; //T->G
+
+	}
+	
+	@Override
+	public boolean canHandleDataType(DataType dataType) throws Exception {
+		if (dataType instanceof Nucleotide) {
+			return true;
+		}
+		throw new Exception("Can only handle nucleotide data");
+	}
+}
---
+++ trunk/src/beast/math/distributions/RBPrior.java
@@ -0,0 +1,36 @@
+package beast.math.distributions;
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.Valuable;
+import beast.core.Input.Validate;
+import beast.core.parameter.IntegerParameter;
+
+@Description("Prior for Reversible-jump Based (RB) substitution model, applies prior only to the rates that are in use")
+public class RBPrior extends Prior {
+	public Input<IntegerParameter> countInput = new Input<IntegerParameter>("count","count parameter indicating the nr of rates to use", Validate.REQUIRED);
+
+	private IntegerParameter counts;
+	
+	@Override
+	public void initAndValidate() {
+		super.initAndValidate();
+		counts = countInput.get();
+	}
+	
+	@Override
+	public double calculateLogP() throws Exception {
+		Valuable x = m_x.get();
+		int dim = counts.getValue();
+		double fOffset = m_dist.m_offset.get();
+		logP = 0;
+		for (int i = 0; i < dim; i++) {
+			double fX = x.getArrayValue(i) - fOffset;
+			//fLogP += Math.log(density(fX));
+			logP += m_dist.logDensity(fX);
+		}
+		return logP;
+	}
+
+
+}
