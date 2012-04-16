f2e8265e5e0c7211b16626c7f50779564ecc7b37
higgs.ml@gmail.com <higgs.ml@gmail.com@ec332d28-3f27-85f1-ef98-7be09d9b0f74>
2012-04-16 02:18:06 +0000

    partition finding model

---
+++ src/beast/evolution/likelihood/PartitionProviderByCodon.java
@@ -0,0 +1,68 @@
+package beast.evolution.likelihood;
+
+import java.util.ArrayList;
+
+import beast.core.Description;
+
+@Description("Splits alignment into partititions by lengths, and subdivides these partitions by codon position")
+public class PartitionProviderByCodon extends PartitionProvider {
+
+	
+	public void initAndValidate() throws Exception {
+    	partitionLengths = partitionLengthsInput.get();
+    	if (partitionLengths.getDimension() * 3 != m_pSiteModel.get().size()) {
+    		throw new Exception("nr of site models must be 3 times nr of partitions defined trough partitionLengths");
+    	}
+    			
+    	siteCount = alignment.get().getSiteCount();
+    	patternIndex = new int[siteCount];
+    	patternCount = alignment.get().getPatternCount();
+    	for (int i = 0; i < siteCount; i++) {
+        	patternIndex[i] = alignment.get().getPatternIndex(i);
+    	}
+    	
+    	patternIndicators = new ArrayList[m_pSiteModel.get().size()];
+    	for (int i = 0; i < patternIndicators.length; i++) {
+    		patternIndicators[i] = new ArrayList<Integer>();
+    	}
+
+    	// sanity check: each site must be accounted for
+    	int sum = 0;
+    	for (int i = 0; i < partitionLengths.getDimension(); i++) {
+    		sum += partitionLengths.getValue(i);
+    	}
+    	if (sum * 3 != siteCount + (siteCount % 3 == 0? 0 : 3-siteCount % 3)) {
+    		throw new Exception("nr of sites does not match partitions lengths (sum=" + sum + "*3 != siteCount=" + siteCount+")");
+    	}
+    	
+//    	weights = new int[patternIndicators.length][patternCount];
+//    	int [] patternWeights = alignment.get().getWeights();
+//    	for (int i = 0; i < patternCount; i++) {
+//    		patternIndicators[i%2].add(i);
+//    		weights[i%2][i] = patternWeights[i];
+//    	}
+
+    	update();
+    }
+    
+    void update() {
+    	for (int i = 0; i < patternIndicators.length; i++) {
+    		patternIndicators[i].clear();
+    	}
+    	weights = new int[patternIndicators.length][patternCount];
+    	int partition = 0;
+    	int upper = partitionLengths.getValue(partition) * 3;
+    	for (int i = 0; i < siteCount; i++) {
+        	while (i >= upper) {
+        		partition+=3;
+        		upper += partitionLengths.getValue(partition/3) * 3;
+        	}
+    		int pattern = patternIndex[i];
+    		if (weights[partition + i % 3][pattern] == 0) {
+    			patternIndicators[partition + i % 3].add(pattern);
+    		}
+    		weights[partition + i % 3][pattern]++;
+    	}
+    	needsUpdate = false;
+    }
+}
