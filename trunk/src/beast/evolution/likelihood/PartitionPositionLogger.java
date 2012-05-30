1cf03ab5da61d3f984e5452d9344cf14baa4ed6b
higgs.ml@gmail.com <higgs.ml@gmail.com@ec332d28-3f27-85f1-ef98-7be09d9b0f74>
2012-05-30 06:55:54 +0000

    add partition position logger

---
+++ trunk/src/beast/evolution/likelihood/PartitionPositionLogger.java
@@ -0,0 +1,44 @@
+package beast.evolution.likelihood;
+
+import java.io.PrintStream;
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.Loggable;
+import beast.core.Plugin;
+import beast.core.Input.Validate;
+import beast.core.parameter.IntegerParameter;
+
+@Description("Logs the position of partion boundaries based on partition lengths")
+public class PartitionPositionLogger extends Plugin implements Loggable {
+    public Input<IntegerParameter> partitionLengthsInput = new Input<IntegerParameter>("partitionLengths", "consecutive length of partitions", Validate.REQUIRED);
+    
+    IntegerParameter partitionLengths;
+    
+    @Override
+    public void initAndValidate() throws Exception {
+    	partitionLengths = partitionLengthsInput.get();
+    }
+
+	@Override
+	public void init(PrintStream out) throws Exception {
+		for (int i = 0; i< partitionLengths.getDimension() - 1; i++) {
+			out.append(partitionLengths.getID() + ".pos." + (i+1) + "\t");
+		}
+	}
+
+	@Override
+	public void log(int nSample, PrintStream out) {
+		int sum = 0;
+		for (int i = 0; i< partitionLengths.getDimension() - 1; i++) {
+			sum += partitionLengths.getValue(i);
+			out.append(sum + "\t");
+		}
+	}
+
+	@Override
+	public void close(PrintStream out) {
+		// nothing to do
+	}
+
+}
