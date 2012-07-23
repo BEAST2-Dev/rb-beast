71ff4f9eba673d4581fb6d1708012dda3a2a2b3a
higgs.ml <higgs.ml@ec332d28-3f27-85f1-ef98-7be09d9b0f74>
2012-07-23 03:46:51 +0000

    add variable selection (VS) model, add tests

---
+++ trunk/src/test/beast/evolution/substitutionmodel/RBHKYTest.java
@@ -0,0 +1,42 @@
+package test.beast.evolution.substitutionmodel;
+
+import test.beast.evolution.substmodel.HKYTest;
+import beast.core.Description;
+import beast.core.parameter.IntegerParameter;
+import beast.core.parameter.RealParameter;
+import beast.evolution.substitutionmodel.Frequencies;
+import beast.evolution.substitutionmodel.RB;
+
+@Description("Test HKY matrix exponentiation")
+public class RBHKYTest extends HKYTest {
+
+	Instance[] all = {test2, test1, test0};
+
+    public void testHKY() throws Exception {
+        for (Instance test : all) {
+
+            RealParameter f = new RealParameter(test.getPi());
+
+            Frequencies freqs = new Frequencies();
+            freqs.initByName("frequencies", f, "estimate", false);
+
+            RB rbmodel = new RB();
+            Double [] rates = new Double[5];
+            rates[0] = 1.0/test.getKappa();
+            rbmodel.initByName("count", new IntegerParameter("1"), 
+            		"rates", new RealParameter(rates), 
+            		"frequencies", freqs);
+
+            double distance = test.getDistance();
+
+            double[] mat = new double[4 * 4];
+            rbmodel.getTransitionProbabilities(null, distance, 0, 1, mat);
+            final double[] result = test.getExpectedResult();
+
+            for (int k = 0; k < mat.length; ++k) {
+                assertEquals(mat[k], result[k], 1e-10);
+                System.out.println(k + " : " + (mat[k] - result[k]));
+            }
+        }
+    }
+}
