1edc60b07d43cf0b8e859d1f985d069cebfb9c23
higgs.ml <higgs.ml@ec332d28-3f27-85f1-ef98-7be09d9b0f74>
2013-09-30 02:01:03 +0000

    sync with beast2

--- trunk/src/beast/evolution/operators/RBScaleOperator.java
+++ trunk/src/beast/evolution/operators/RBScaleOperator.java
@@ -79,7 +79,7 @@ public class RBScaleOperator extends ScaleOperator {
 
                 // which position to scale
                 int index;
-                final BooleanParameter indicators = ndicatorInput.get();
+                final BooleanParameter indicators = indicatorInput.get();
                 if ( indicators != null ) {
                     final int nDim = indicators.getDimension();
                     Boolean [] indicator = indicators.getValues();
